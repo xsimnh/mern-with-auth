3791e44cf778b3641bbd41535025e0ce328132b9
xsimnh <1040562905@qq.com>
2023-04-07 10:12:42 +0800

    using HashRouter

--- client/index.tsx
+++ client/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.less";
 import Router from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Router />
-  </BrowserRouter>
+  </HashRouter>
 );
