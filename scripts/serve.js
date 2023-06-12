93b8b56323cd64c9ce320ecde76e56166c6ec0a2
xsimnh <1040562905@qq.com>
2023-06-12 10:55:05 +0800

    update serve.js

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const compression = require("compression");
 const proxy = require("express-http-proxy");
-const path = require("path");
 const https = require("https");
 const fs = require("fs");
 
@@ -11,22 +10,36 @@ const app = express();
 app.use(compression());
 app.use(express.static(appPath.wwwroot));
 app.use("/api", proxy("http://localhost:9000"));
-
-app.get("/*", (req, res) => {
-  res.sendFile(appPath.get("wwwroot/index.html"));
+app.get("*", function (req, res) {
+  res.sendFile("index.html", { root: appPath.wwwroot });
 });
 
-const host = "localhost";
+// app.listen(7100);
+
 const options = {
-  key: fs.readFileSync(appPath.get("cert/" + host + ".key")),
-  cert: fs.readFileSync(appPath.get("cert/" + host + ".cert")),
+  key: fs.readFileSync(appPath.get("cert/localhost.key")),
+  cert: fs.readFileSync(appPath.get("cert/localhost.cert")),
   requestCert: false,
   rejectUnauthorized: false,
 };
 
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
+}
+const host = "localhost";
+const port = normalizePort(process.env.PORT || "7100");
 const server = https.createServer(options, app);
-server.listen(7100, host, function () {
-  const { address: host, port } = server.address();
+server.listen(port, host, function () {
+  // const { address: host, port } = server.address();
 
   console.log("server url is: https://%s:%s", host, port);
 });
